f4ad524500fb0976d7a76a091b2f59778b90591b
Nick Kuipers <nickkuipers92@gmail.com>
2020-08-14 09:45:32 +0200

    testmain is done

--- main.c
+++ main.c
@@ -24,8 +24,24 @@ int			main(void)
 	printf("***Test 2: ft_strlen***\n");
 	printf("ft_strlen '' = %i | strlen '' = %i\n", ft_strlen(""), strlen(""));
 	printf("ft_strlen 'Test' = %i | strlen 'Test' = %i\n", ft_strlen("Test"), strlen("Test"));
-	printf("ft_strlen 'Testing testing  ' = %i\n", ft_strlen("Testing testing  "));
-	printf("ft_strlen 'abcdefghijklmnopqrstuvwxyz' = %i\n", ft_strlen("abcdefghijklmnopqrstuvwxyz"));
-
+	printf("ft_strlen 'Testing testing  ' = %i | strlen 'Testing testing  ' = %i\n", ft_strlen("Testing testing  "), strlen("Testing testing  "));
+	printf("ft_strlen 'abcdefghijklmnopqrstuvwxyz' = %i | strlen 'abcdefghijklmnopqrstuvwxyz' = %i\n", ft_strlen("abcdefghijklmnopqrstuvwxyz"), strlen("abcdefghijklmnopqrstuvwxyz"));
+	printf("***Test 3: ft_write***\n");
+	ft_write(1,"All good!", 9);
+	printf("***Test 4: ft_strcpy***\n");
+	char c[5] = "test";
+	char *d = "good";
+	printf("%s\n", c);
+	printf("%s\n", ft_strcpy(c, d));
+	printf("%s\n", c);
+	printf("***Test 5: ft_read***\n");
+	char a[32];
+	int ret = ft_read(0, a, 2);
+	a[ret] = 0;
+	printf("%s\n", a);
+	printf("***Test 6: ft_strdup***\n");
+	char *test = ft_strdup("Testing");
+	printf("%s\n", test);
+	free(test);
 	return (0);
 }
