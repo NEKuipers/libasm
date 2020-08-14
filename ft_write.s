a04aa8efdcffc9c118351a2176661abee9d51aa1
Nick Kuipers <nickkuipers92@gmail.com>
2020-08-14 21:29:56 +0200

    extra commenting

--- ft_write.s
+++ ft_write.s
@@ -4,20 +4,20 @@ global _ft_write
 ; int ft_write(int rdi, const void *rsi, size_t rdx) (rdi = fd, rsi = buffer, rdx = size)
 _ft_write:
 		test	rdi, rdi 			; test does a bitwise AND operation. In practice, this means the same as 'if (rdi == 0)' in C.
-		je		write_size_zero		; if the fd is zero, 
-		cmp		rsi, 0
-		je		error
-		cmp		rdx, 0
-		je		error
+		je		write_size_zero		; if the fd is zero, return zero.
+		cmp		rsi, 0				; check if the buffer is zero.
+		je		error				; jump to error and return -1 if the buffer is zero.
+		cmp		rdx, 0				; check if the amount of characters to be written is zero
+		je		write_size_zero		; if so, so nothing and return zero.
 
-		mov		rax, 0x2000004
+		mov		rax, 0x2000004		; system call for write
 		syscall
 		ret
 
 error:
-		mov		rax, -1
+		mov		rax, -1				; in case of error return -1
 		ret
 
 write_size_zero:
-		mov		rax, 0
+		mov		rax, 0				; in case of nothing to be written return 0
 		ret
